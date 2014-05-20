d1b6f0b41d662151b764c123d5456371658cc61c
g_rocket <g.rocket1@gmail.com>
2014-05-20 16:10:38 -0400

    Make it work (sort of).  Still a bunch of logic bugs left

--- src/net/clonecomputers/lab/g2048/G2048.java
+++ src/net/clonecomputers/lab/g2048/G2048.java
@@ -2,6 +2,8 @@ package net.clonecomputers.lab.g2048;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.util.*;
+import java.util.List;
 
 import javax.swing.*;
 
@@ -21,24 +23,23 @@ public class G2048 extends JPanel implements Runnable {
 	@Override
 	public void run() {
 		this.setLayout(new GridLayout(numbers.length, numbers[0].length));
-		for(int i = 0; i < numbers.length; i++) for(int j= 0; j < numbers[i].length; j++) {
-			numbers[i][j] = 0;
-			labels[i][j] = new JLabel(Integer.toString(numbers[i][j]));
-			labels[i][j].setMinimumSize(new Dimension(40,40));
-			labels[i][j].setPreferredSize(new Dimension(40,40));
-			labels[i][j].setAlignmentX(JLabel.CENTER_ALIGNMENT);
-			labels[i][j].setAlignmentY(JLabel.CENTER_ALIGNMENT);
+		for(int x = 0; x < numbers.length; x++) for(int y = 0; y < numbers[x].length; y++) {
+			numbers[x][y] = 0;
+			labels[x][y] = new JLabel(Integer.toString(numbers[x][y]));
+			labels[x][y].setMinimumSize(new Dimension(40,40));
+			labels[x][y].setPreferredSize(new Dimension(40,40));
+			labels[x][y].setAlignmentX(JLabel.CENTER_ALIGNMENT);
+			labels[x][y].setAlignmentY(JLabel.CENTER_ALIGNMENT);
 			Font f = new Font("Monospaced",Font.PLAIN,24);
 			// make font centered
-			labels[i][j].setFont(f);
-			labels[i][j].setBorder(BorderFactory.createLineBorder(Color.BLACK));
-			this.add(labels[i][j]);
+			labels[x][y].setFont(f);
+			labels[x][y].setBorder(BorderFactory.createLineBorder(Color.BLACK));
 		}
+		
+		for(int y = numbers[0].length-1; y >= 0; y--) for(int x = 0; x < numbers.length; x++) this.add(labels[x][y]);
 		
 		KeyListener kl = new KeyAdapter() {
 			@Override public void keyTyped(KeyEvent e) {
-				System.out.println("Got a keypress");
-				System.out.println("it was "+e.getKeyChar());
 				switch(e.getKeyChar()) {
 				case 'w': move(Dir.UP); break;
 				case 'a': move(Dir.LEFT); break;
@@ -69,6 +70,34 @@ public class G2048 extends JPanel implements Runnable {
 	
 	private void move(Dir d) {
 		System.out.println("moving "+d);
+		
+		boolean moved = false;
+		List<Integer> movedPoints = new ArrayList<Integer>();
+		for(int x = 0; x < numbers.length; x++) for(int y = 0; y < numbers[x].length; y++) {
+			if(movedPoints.contains(x + y*numbers.length)) continue;
+			if(numbers[x][y] == 0) continue;
+			if(numbers[x][y] == d.inDir(x,y,numbers)) {
+				numbers[x+d.dx][y+d.dy] += numbers[x][y];
+				numbers[x][y] = 0;
+				movedPoints.add((x+d.dx) + (y+d.dy)*numbers.length);
+				moved = true;
+			} else if(d.inDir(x,y,numbers) == 0) {
+				numbers[x+d.dx][y+d.dy] = numbers[x][y];
+				numbers[x][y] = 0;
+				movedPoints.add((x+d.dx) + (y+d.dy)*numbers.length);
+				moved = true;
+			}
+		}
+		if(moved) {
+			int x,y;
+			do {
+				x = rand(); y = rand();
+			} while(numbers[x][y] != 0);
+			numbers[x][y] = Math.random() > .1? 2: 4;
+		} else {
+			JOptionPane.showMessageDialog(this, "You lose!");
+		}
+		
 		updateDisplay();
 	}
 	
@@ -82,6 +111,16 @@ public class G2048 extends JPanel implements Runnable {
 		private Dir(int dx, int dy) {
 			this.dx = dx;
 			this.dy = dy;
+		}
+		
+		private int inDir(int x, int y, int[][] numbers) {
+			x += dx;
+			y += dy;
+			if(x < 0 || y < 0 || x >= numbers.length || y >= numbers[0].length) {
+				return -1;
+			} else {
+				return numbers[x][y];
+			}
 		}
 	}
 
